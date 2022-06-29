986cb5f3c11cca23c603fd1ab61be4a3ae58b2fc
Chase <chasedemaster.tech@gmail.com>
2022-06-28 19:18:11 -0500

    commit tree 1734ce7b1ad657817dd35e6f9965f0356e3b941b

--- src/components/AppPractice.js
+++ src/components/AppPractice.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState} from 'react';
 
 function App() {
     const [random, setRandom] = useState("");
     function fetchRandomDog() {
         fetch("https://dog.ceo/api/breeds/image/random")
         .then((res) => res.json())
-        .then((dogImage) => setRandom(dogImage))
+        .then((dogImage) => setRandom(dogImage.message))
     }
-
+    
     useEffect(fetchRandomDog, [])
     return(
         <div className='App'>
             <p>Loading...</p>
             <img src={random} alt="dog"/>
-        </div>
+        </div>      
         )
     }
-
     export default App;
